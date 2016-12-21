570c7cb808ddea17e6388ad3d41af74c92bcdca7
Nano-Hub <nano-971@hotmail.com>
2016-12-21 12:03:57 +0100

    Update test_webcam.cpp

--- test_webcam.cpp
+++ test_webcam.cpp
@@ -9,11 +9,8 @@ using namespace std;
 int main(int argc, char** argv)
 {
     QApplication a(argc, argv);
-    qDebug()<<"yo";
     MainWindow w;
-    qDebug()<<"yo";
     w.setWindowTitle("Projet 3D - (Blanc - Alder)");
-    qDebug()<<"yo";
     w.show();
     return a.exec();
     return 0;
